c49cacb11b53c052b658c760c376556c9010400e
Divide-By-0 <aayushg@mit.edu>
2019-10-06 16:04:06 -0400

    code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# cli-blackjack" 
+CLI Blackjack
+
+See design doc at https://docs.google.com/document/d/1vBVhxSbb0ZtGFHa7ODjKZNOryw7zDM7_78ziUfiSw3I/edit?usp=sharing.
+Clone and run with 
+``` python3 blackjack.py ```
--- blackjack.py
+++ blackjack.py
@@ -25,9 +25,6 @@ cardNums = {'A': 1,
 allCards = []
 deck = []
 
-def displayPot(amount):
-	print("You have " + str(amount) + " chips remaining.")
-
 def resetDeck():
 	global deck
 	for dNum in range(0, 6):
@@ -41,40 +38,108 @@ def calcTotal(cards):
 	sum2 = -1
 	hasAce = False
 	for card in cards:
-		sum1 += cardNums[allCards[card][0]]
-		if(allCards[card][0] == 'A'):
+		sum1 += cardNums[card[0]]
+		if(card[0] == 'A'):
 			hasAce = True
 	if(hasAce):	# if you have two aces, they add to 2, 12, or 22 -- don't need the last one 
 		sum2 = sum1 + 10
-	if(sum2 > 21 or sum2 < 0):
-		return [sum1]
-	return [sum1, sum2]
+		return [sum1, sum2] #returns two if they have an ace	
+	return [sum1]
+	
 
 def parseTotal(cards):
 	total = calcTotal(cards)
-	if(len(total) > 1):
+	if(len(total) > 1 and total[1] <= 21):
 		return str(total[0]) + " or " + str(total[1])
 	else:
 		return str(total[0])
 
-def dealCards():
-	if(len(deck) < 3 * 52):
-		resetDeck()
+def dealInitialCards():
 	global myCards
 	global dealerCards
-	cards = []
-	nextCard = -1
+	global deck
+	if(len(deck) < 3 * 52):
+		resetDeck()
 	#deck is shuffled
 	myCards = [deck[0], deck[1]]
 	dealerCards = [deck[2], deck[3]]
 	deck = deck[4:]
 
-def getCard(num):
-	return allCards[num][0] + allCards[num][1]
+def getCard(card):
+	return card[0] + card[1]
 
-def displayGame():
-	print("Dealer has: ?, " + getCard(dealerCards[1]))
-	print("You have: " + getCard(myCards[0]) + ", " + getCard(myCards[1]) + " (totals to " + parseTotal(myCards) + ")")
+def displayGame(showDealerCards = False):
+	global chips
+	if(not inGame):
+		print("\nYou have " + str(chips) + " to bet. Use \'bet <amount>\' to start playing!")
+		return
+	myCardString = ""
+	for card in myCards:
+		myCardString += getCard(card) + ", "
+	myCardString = myCardString[:-2] #remove last comma
+
+	dealerCardString = ""
+	for card in dealerCards:
+		dealerCardString += getCard(card) + ", "
+	dealerCardString = dealerCardString[:-2] #remove last comma
+
+	if(showDealerCards):
+		print("Dealer has: " + dealerCardString + " (totals to " + parseTotal(dealerCards) + ")")
+	else:
+		print("Dealer has: ?, " + getCard(dealerCards[1]))
+	print("You have: " + myCardString + " (totals to " + parseTotal(myCards) + ")")
+
+def getFinalScores():
+	dTotal = calcTotal(dealerCards)
+	myTotal = calcTotal(myCards)
+	dFinal = dTotal[0]
+	myFinal = myTotal[0]
+	if((len(dTotal) > 1) and (dTotal[1] <= 21)):
+		dFinal = dTotal[1]
+	if((len(myTotal) > 1) and (myTotal[1] <= 21)):
+		myFinal = myTotal[1]
+	return [dFinal, myFinal]
+
+def clearCurrentGame():
+	global myPot
+	global inGame
+	global myCards
+	global dealerCards
+	myPot = 0
+	myCards = []
+	dealerCards = []
+	inGame = False
+	
+def dealerPlay():
+	global deck
+	global chips
+	total = calcTotal(dealerCards)
+	while(total[-1] <= 16):
+		print("\nDealer hits!")
+		dealerCards.append(deck[0])
+		deck = deck[1:]
+		displayGame(1)
+		total = calcTotal(dealerCards)
+
+	print("\nDealer stands!")
+	displayGame(1)
+	
+	scores = getFinalScores()
+	if(scores[0] > 21):
+		print("\nDealer busts!")
+	if(scores[0] < scores[1] or scores[0] > 21):
+		print("\nYou win! You get " + str(myPot * 2) + " chips!")
+		chips += myPot * 2
+	elif(scores[1] < scores[0]):
+		print("\nYou lose!")
+		# chips += myPot * 2
+	elif(scores[0] == scores[1]):
+		print("\nYou tie! You get " + str(myPot) + " chips!")
+		chips += myPot
+	clearCurrentGame()
+
+def bust():
+	clearCurrentGame()
 
 class BlackjackCmd(cmd.Cmd):
 	prompt = '\n> '
@@ -90,8 +155,11 @@ class BlackjackCmd(cmd.Cmd):
 	def do_bet(self, arg):
 		global myPot
 		global chips
+		global inGame
 		""""bet <amount> - Start the game with this bet."""
-		# put this value in a more suitably named variable
+		if(inGame):
+			print("In game! Must hit or stand.")
+			return
 		if(not arg.isnumeric()):
 			print("You must bet a numeric value.")
 			return
@@ -104,7 +172,48 @@ class BlackjackCmd(cmd.Cmd):
 		myPot += amount
 		inGame = True
 		print("Successfully bet " + str(myPot) + " chips. You have " + str(chips) + " left.")
-		dealCards()
+		dealInitialCards()
+		displayGame()
+		total = calcTotal(myCards)
+		if(total[0] == 21 or (len(total) > 1 and total[1] == 21)):
+			print("Blackjack!")
+			dealerPlay()
+			displayGame()
+
+	def do_hit(self, arg):
+		global inGame
+		global myCards
+		global deck
+		if(not inGame):
+			print("Cannot hit if not in game!")
+			return
+		total = calcTotal(myCards)
+		if(total[0] >= 21):
+			print("Cannot hit!")
+			return
+		global deck
+		myCards.append(deck[0])
+		deck = deck[1:]
+		total = calcTotal(myCards)
+		print("You hit!")
+		if(total[0] > 21):
+			displayGame()
+			print("Bust!")
+			bust()
+		if(total[0] == 21 or (len(total) > 1 and total[1] == 21)):
+			displayGame()
+			print("Blackjack!")
+			dealerPlay()
+		displayGame()
+		return
+
+	def do_stand(self, arg):
+		global inGame
+		if(not inGame):
+			print("Cannot stand if not in game!")
+			return
+		displayGame(1)
+		dealerPlay()
 		displayGame()
 
 	def help_bet(self):
@@ -118,11 +227,11 @@ if __name__ == '__main__':
 		for suit in suits:
 			allCards.append([num, suit])
 
-	print('Blackjack!')
+	print('Play Blackjack!')
 	print('====================')
 	print()
 	print('(Type "help" for commands.)')
 	print()
-	displayPot(chips)
+	displayGame()
 	BlackjackCmd().cmdloop()
 	print('Thanks for playing!')
